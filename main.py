1cdb50019ad91f75319f8b5b19444ee941424893
nandKadivar <kadivarnand007@gmail.com>
2020-10-05 20:36:51 +0530

    Updated: main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from view import GUI
+from controller import Controller
 from  model import evaluateExpression
 # Client code
 def main():
@@ -14,6 +15,7 @@ def main():
     view.show()
 
     model = evaluateExpression
+    Controller(model=model, view=view)
     
     sys.exit(pycalc.exec_())
 
